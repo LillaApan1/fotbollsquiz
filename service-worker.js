80d6201873bb3b3652ad4b804c12a6f2cb4c888f
LillaApan1 <cupic.branimir@gmail.com>
2026-02-20 17:10:49 +0100

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // 🔥 Automatisk versionshantering – ny version vid varje deploy
-const CACHE_NAME = "quiz-cache-v3";
+const CACHE_NAME = "quiz-cache-v4";
 
 // 📩 Ta emot meddelande från appen om att hoppa över vänteläge
 self.addEventListener("message", event => {
