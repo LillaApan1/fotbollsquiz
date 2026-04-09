34d8807da3b23a7da1fd588d808c2664ecd1552b
LillaApan1 <cupic.branimir@gmail.com>
2026-04-09 09:32:08 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // 🔥 Automatisk versionshantering – höj version vid varje deploy
-const CACHE_NAME = "quiz-cache-v40";
+const CACHE_NAME = "quiz-cache-v41";
 
 // Filer som ska finnas offline för själva quizet
 const APP_SHELL = [
